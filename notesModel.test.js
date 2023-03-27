5a3d22cf570e41f8292ac345a6a753f009ab811f
Dilan Patel <patel_dilan@hotmail.com>
2023-03-27 15:16:05 +0100

    Refactored to include a 'beforeEach' function to initiate a new instance before each test

--- notesModel.test.js
+++ notesModel.test.js
@@ -2,21 +2,24 @@ const NotesModel = require('./notesModel');
 
 describe('NotesModel', () => {
 
+  let notesModel;
+
+  beforeEach(() => {
+    notesModel = new NotesModel();
+  });
+
   it('constructs with an empty notes list', () => {
-    // const notesModel = new NotesModel();
     expect(notesModel).toBeInstanceOf(NotesModel);
     expect(notesModel.getNotes()).toEqual([]);
   });
 
   it('add a note to the list', () => {
-    const notesModel = new NotesModel();
     notesModel.addNote('Buy milk');
     notesModel.addNote('Go to the gym');
     expect(notesModel.getNotes()).toEqual(['Buy milk', 'Go to the gym']);
   });
 
   it('resets the list to empty when called', () => {
-    const notesModel = new NotesModel();
     notesModel.addNote('Buy milk');
     notesModel.addNote('Go to the gym');
     notesModel.reset();
