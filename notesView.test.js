edce43fea910ff8962a1889cc4b1a0c9e63d7b35
Dilan Patel <patel_dilan@hotmail.com>
2023-03-31 11:06:45 +0100

    Refactored test to use a beforeEach instance

--- notesView.test.js
+++ notesView.test.js
@@ -8,25 +8,34 @@ const NotesView = require('./notesView');
 
 describe ('NotesView', () => {
 
-  it('displays all notes', () => {
+  let model;
+  let view;
+
+  beforeEach(() => {
     document.body.innerHTML = fs.readFileSync('./index.html');
-    const model = new NotesModel();
-    const view = new NotesView(model);
+    model = new NotesModel();
+    view = new NotesView(model);
+  });
+
+  it('displays all notes', () => {
     model.addNote('First note');
     model.addNote('Second note');
+
     view.displayNotes();
+
     expect(document.querySelectorAll('div.note').length).toBe(2);
   });
 
   it('adds a new note and displays it', () => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
-    const model = new NotesModel();
-    const view = new NotesView(model);
     const addNoteButtonEl = document.querySelector('#add-note-button');
+
     const inputEl = document.querySelector('#note-input');
     inputEl.value = 'secret note';
+
     addNoteButtonEl.click();
+
     const notes = document.querySelectorAll('.note');
+    
     expect(notes.length).toBe(1);
     expect(notes[0].textContent).toEqual('secret note');
   });
